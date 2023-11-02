86686506a825a725b0bd390a3ba55e718aac2cae
M1k3-404 <muthula.alwis@gmail.com>
2023-11-02 12:20:28 +0530

    Price Update

--- src/logic/calculateTotal.js
+++ src/logic/calculateTotal.js
@@ -1,12 +1,16 @@
 function CalculateTotal(ticketCount, promoCode) {
-    var ticketPrice = 2500;
+    var ticketPrice = 2000;
 
-    if (new Date() < new Date('2023-11-04T00:00:00Z')) {
-        ticketPrice = 1800;
+    if (new Date() < new Date('2023-11-08T00:00:00Z')) {
+        ticketPrice -= 400;
     }
 
     if (promoCode === 'R2KICSC' && ticketCount == 6) {
-        ticketPrice = 1500;
+        ticketPrice = 1200;
+    } else if (promoCode === 'FLEXLK' && ticketCount == 3) {
+        ticketPrice = 1400;
+    } else if (promoCode === 'R2G2022' && ticketCount >= 3) {
+        ticketPrice = 1400;
     }
 
     const totalTicketPrice = ticketCount * ticketPrice;
