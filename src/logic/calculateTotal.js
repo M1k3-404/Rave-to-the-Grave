5c3b2ed1a89d7a5054e2343171465afc52605634
M1k3-404 <muthula.alwis@gmail.com>
2023-11-02 16:08:51 +0530

    R2KICSC Price Update

--- src/logic/calculateTotal.js
+++ src/logic/calculateTotal.js
@@ -6,7 +6,7 @@ function CalculateTotal(ticketCount, promoCode) {
     }
 
     if (promoCode === 'R2KICSC' && ticketCount == 6) {
-        ticketPrice = 1200;
+        ticketPrice = 1300;
     } else if (promoCode === 'FLEXLK' && ticketCount == 3) {
         ticketPrice = 1400;
     } else if (promoCode === 'R2G2022' && ticketCount >= 3) {
